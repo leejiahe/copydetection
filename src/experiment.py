ac7a1d57640889b4354458f87e7ae4ce39315a4e
Lee Jiahe <90043356+leejiahe@users.noreply.github.com>
2022-03-11 09:43:18 +0800

    added experiment pipeline

---
+++ src/experiment.py
@@ -0,0 +1,49 @@
+import os
+from typing import List, Optional
+
+import hydra
+from omegaconf import DictConfig
+from pytorch_lightning import (
+    Callback,
+    LightningDataModule,
+    LightningModule,
+    Trainer,
+    seed_everything,
+)
+from pytorch_lightning.loggers import LightningLoggerBase
+
+from src import utils
+
+log = utils.get_logger(__name__)
+
+
+def train(config: DictConfig) -> Optional[float]:
+    """Contains the training pipeline.
+    Can additionally evaluate model on a testset, using best weights achieved during training.
+
+    Args:
+        config (DictConfig): Configuration composed by Hydra.
+
+    Returns:
+        Optional[float]: Metric score for hyperparameter optimization.
+    """
+
+    # Set seed for random number generators in pytorch, numpy and python.random
+    if config.get("seed"):
+        seed_everything(config.seed, workers = True)
+
+    # Convert relative ckpt path to absolute path if necessary
+    ckpt_path = config.trainer.get("resume_from_checkpoint")
+    if ckpt_path and not os.path.isabs(ckpt_path):
+        config.trainer.resume_from_checkpoint = os.path.join(
+            hydra.utils.get_original_cwd(), ckpt_path
+        )
+
+    # Init lightning datamodule
+    log.info(f"Instantiating datamodule <{config.datamodule._target_}>")
+    datamodule: LightningDataModule = hydra.utils.instantiate(config.datamodule)
+    datamodule.setup()
+    datamodule.setup_folds()
+    #datamodule.prepare_data()
+
+    b =  next(iter(datamodule.train_dataloader()))
