d4f48dd7e0fd24562b2abbe1571a8384afe08b20
leejiahe <leejiahe@u.nus.edu>
2022-03-15 10:32:32 +0800

    to main

--- src/datamodules/copydetect_datamodule.py
+++ src/datamodules/copydetect_datamodule.py
@@ -1,4 +1,5 @@
 import os
+from matplotlib import image
 import numpy as np
 from PIL import Image
 from dataclasses import dataclass
@@ -9,26 +10,26 @@ from sklearn.model_selection import KFold
 from torch.utils.data import Dataset
 from torch.utils.data.dataloader import DataLoader
 from torch.utils.data.dataset import Subset
-from torchvision.io import read_image
 from torchvision.transforms import transforms
+from pytorch_lightning import LightningDataModule
 import augly.image as imaugs
-
-from ..utils.kfold import BaseKFoldDataModule
-from ..utils import download_and_unzip
 
 from components.augmentation import *
 
 class CopyDetectDataset(Dataset):
     def __init__(self,
                  image_dir: str,
-                 n_upper: int,
+                 image_size: int,
+                 pretrain: bool = False,
+                 n_crops: int = 1,
+                 n_upper: int = 2,
                  n_lower: int = 1,
-                 training_image_dir: str = None,
-                 pretrain: bool = False):
+                 overlay_image_dir: str = None):
         
         assert(os.path.exists(image_dir))
         self.image_files = [os.path.join(image_dir, f) for f in os.listdir(image_dir) if os.path.isfile(os.path.join(image_dir, f))]
         self.pretrain = pretrain
+        self.n_crops = n_crops
         
         if pretrain:
             transforms_list = [OverlayRandomStripes(),
@@ -38,8 +39,8 @@ class CopyDetectDataset(Dataset):
                                OverlayRandomText(),
                                RandomSaturation(),
                                ApplyRandomPILFilter(),
-                               OverlayOntoRandomBackgroundImage(training_image_dir),
-                               OverlayOntoRandomForegroundImage(training_image_dir),
+                               OverlayOntoRandomBackgroundImage(overlay_image_dir),
+                               OverlayOntoRandomForegroundImage(overlay_image_dir),
                                RandomShufflePixels(),
                                OverlayOntoRandomScreenshot(),
                                RandomPadSquare(),
@@ -58,7 +59,7 @@ class CopyDetectDataset(Dataset):
             self.aug = N_Compositions(transforms_list, n_upper = n_upper, n_lower = n_lower)
 
         self.trfm = transforms.Compose([transforms.ToTensor(),
-                                        transforms.Resize((224, 224)),
+                                        transforms.Resize((image_size, image_size)),
                                         transforms.Normalize(IMAGENET_MEAN, IMAGENET_SDEV)])
         
     def __len__(self) -> int:
@@ -69,59 +70,65 @@ class CopyDetectDataset(Dataset):
         image = Image.open(image_path)
 
         if self.pretrain:
-            return self.trfm(image), self.trfm(self.aug(image))
+            imgs = []
+            for _ in range(self.n_crops):
+                aug_index = index
+                if random.random() > 0.5:
+                    aug_index = random.randint(0, len(self.image_files)) # Get random image file
+                label = torch.tensor(aug_index == index, dtype = torch.long) # label 1: modified copy: aug_index == index 
+                aug_image = Image.open(self.image_files[aug_index])
+                imgs.append((self.trfm(image), self.trfm(self.aug(aug_image)), label))
+            return imgs
         else:
             return self.trfm(image)
         
 
 @dataclass
-class CDKFoldDataModule(BaseKFoldDataModule):
+class CopyDetectDataModule(LightningDataModule):
     training_dir: str
     reference_dir: str
     query_dev_dir: str 
     query_final_dir: str
     num_folds: int
-    train_fold: Optional[Dataset] = None
-    val_fold: Optional[Dataset] = None
     batch_size: int = 128
     num_workers: int = 0
     pin_memory: bool = False
-
-    def prepare_data(self) -> None:
-        for dir in [self.hparams.training_dir, self.hparams.reference_dir, self.hparams.query_dev_dir, self.hparams.query_final_dir]:
-            if os.path.exists(dir) == False:
-                os.mkdir(dir)
-        # download the data.
-        from multiprocessing import Pool, RLock
-        from tqdm import tqdm
-        
-        urls = []
-        urls.append(("https://dl.fbaipublicfiles.com/image_similarity_challenge/public/dev_queries.zip", self.hparams.query_dev_dir))
-        urls.append(("https://dl.fbaipublicfiles.com/image_similarity_challenge/public/final_queries.zip", self.hparams.query_final_dir))
-        urls.extend([(f"https://dl.fbaipublicfiles.com/image_similarity_challenge/public/train_{i}.zip", self.hparams.training_dir) for i in range(20)])
-        urls.extend([(f"https://dl.fbaipublicfiles.com/image_similarity_challenge/public/references_{i}.zip", self.hparams.reference_dir) for i in range(20)])
-
-        with Pool(processes = 8, initargs = (RLock(),), initializer = tqdm.set_lock) as p:
-            jobs = [p.apply_async(download_and_unzip, args = (url, dir, i)) for i, (url, dir) in enumerate(urls)]
-            results = [job.get() for job in jobs]
-        
-        print('Finish downloading all data!')
-            
-        
+    
+    def __post_init__(self):
+        super().__init__()
+        self.save_hyperparameters(logger = False)
+    
     def setup(self, stage: Optional[str] = None) -> None:
         # load the data
-        self.train_dataset = CopyDetectDataset(self.hparams.training_dir, pretrain = True)
-        #self.reference_dataset = CopyDetectDataset(self.hparams.reference_dir, pretrain = False)
-        #self.query_dev_dataset = CopyDetectDataset(self.hparams.query_dev_dir, pretrain = False)
-        #self.query_final_dataset = CopyDetectDataset(self.hparams.query_final_dir, pretrain = False)
+        self.train_dataset = CopyDetectDataset(image_dir = self.hparams.training_dir,
+                                               image_size = self.hparams.image_size,
+                                               pretrain = True,
+                                               n_crops = self.hparams.n_crops,
+                                               n_upper = self.hparams.n_upper,
+                                               n_lower = self.hparams.n_lower,
+                                               overlay_image_dir = self.hparams.training_dir)
+        
+        self.reference_dataset = CopyDetectDataset(image_dir = self.hparams.training_dir,
+                                                   image_size = self.hparams.image_size,
+                                                   pretrain = False)
+        
+        self.query_dev_dataset = CopyDetectDataset(image_dir = self.hparams.training_dir,
+                                                   image_size = self.hparams.image_size,
+                                                   pretrain = False)
+        
+        self.query_final_dataset = CopyDetectDataset(image_dir = self.hparams.training_dir,
+                                                     image_size = self.hparams.image_size,
+                                                     pretrain = False)
+        
+        self.setup_folds()
+        
 
     def setup_folds(self) -> None:
         self.splits = [split for split in KFold(self.hparams.num_folds).split(range(len(self.train_dataset)))]
-
-    def setup_fold_index(self, fold_index: int) -> None:
         train_indices, val_indices = self.splits[fold_index]
         self.train_fold = Subset(self.train_dataset, train_indices)
         self.val_fold = Subset(self.train_dataset, val_indices)
+
 
     def train_dataloader(self) -> DataLoader:
         return DataLoader(dataset = self.train_fold,
@@ -158,6 +165,3 @@ class CDKFoldDataModule(BaseKFoldDataModule):
                           pin_memory = self.hparams.pin_memory,
                           shuffle = False)
         
-    def __post_init__(self):
-        super().__init__()
-        self.save_hyperparameters(logger = False)
